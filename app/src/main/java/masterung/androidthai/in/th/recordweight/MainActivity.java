dd0217329fa45e304de1f3c30218efaadc1611bd
MasterUNG <phrombutr@gmail.com>
2018-01-30 11:58:04 +0700

    Finish 30JAn

--- app/src/main/java/masterung/androidthai/in/th/recordweight/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/recordweight/MainActivity.java
@@ -3,11 +3,19 @@ package masterung.androidthai.in.th.recordweight;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterung.androidthai.in.th.recordweight.utility.MyManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MyManager myManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        myManager = new MyManager(MainActivity.this);
+
+    }   // Main Method
+
+}   // Main Class
